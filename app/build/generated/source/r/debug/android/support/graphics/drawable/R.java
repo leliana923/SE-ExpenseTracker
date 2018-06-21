af4f6a05d2cd16ef2de603386f6f1451b139cfe7
mqkoh <30967444+mqkoh@users.noreply.github.com>
2018-06-21 15:46:08 +0800

    Add entry symbol to log

--- app/build/generated/source/r/debug/android/support/graphics/drawable/R.java
+++ app/build/generated/source/r/debug/android/support/graphics/drawable/R.java
@@ -79,17 +79,17 @@ public final class R {
         public static final int italic = 0x7f090066;
         public static final int line1 = 0x7f09006a;
         public static final int line3 = 0x7f09006b;
-        public static final int normal = 0x7f09007a;
-        public static final int notification_background = 0x7f09007b;
-        public static final int notification_main_column = 0x7f09007c;
-        public static final int notification_main_column_container = 0x7f09007d;
-        public static final int right_icon = 0x7f09008a;
-        public static final int right_side = 0x7f09008b;
-        public static final int tag_transition_group = 0x7f0900b6;
-        public static final int text = 0x7f0900b7;
-        public static final int text2 = 0x7f0900b8;
-        public static final int time = 0x7f0900be;
-        public static final int title = 0x7f0900bf;
+        public static final int normal = 0x7f09007b;
+        public static final int notification_background = 0x7f09007c;
+        public static final int notification_main_column = 0x7f09007d;
+        public static final int notification_main_column_container = 0x7f09007e;
+        public static final int right_icon = 0x7f09008b;
+        public static final int right_side = 0x7f09008c;
+        public static final int tag_transition_group = 0x7f0900b7;
+        public static final int text = 0x7f0900b8;
+        public static final int text2 = 0x7f0900b9;
+        public static final int time = 0x7f0900bf;
+        public static final int title = 0x7f0900c0;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f0a0009;
